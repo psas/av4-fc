436f91eddf5ccb3dfcd13f4a9ce11ec18c941839
Jamey Sharp <jamey@minilop.net>
2016-05-25 00:41:39 -0700

    Use better error handling and abstractions.

--- src/main.rs
+++ src/main.rs
@@ -5,32 +5,62 @@ use byteorder::{BigEndian, ReadBytesExt};
 use i2cdev::core::*;
 use i2cdev::linux::*;
 use std::env;
-use std::io::Cursor;
+use std::error::Error;
+use std::io;
 
-fn setup() -> LinuxI2CDevice {
-	let dev = env::args().nth(1).unwrap();
-	let mut bus = LinuxI2CDevice::new(dev, 0x68).unwrap();
+fn read_reg<E: Error>(bus: &mut I2CDevice<Error=E>, reg: u8, buf: &mut [u8]) -> Result<(), E> {
+	try!(bus.write(&[reg]));
+	bus.read(buf)
+}
 
-	bus.write(&[0x75]);
+fn setup<E: Error + From<io::Error>>(bus: &mut I2CDevice<Error=E>) -> Result<(), E> {
 	let mut buf = [0u8; 1];
-	bus.read(&mut buf);
-	assert!(buf[0] == 0x68);
+	try!(read_reg(bus, 0x75, &mut buf));
+	if buf[0] != 0x68 {
+		return Err(io::Error::new(io::ErrorKind::NotFound, "MPU-9150 WhoAmI returned wrong value").into());
+	}
 
-	bus.write(&[0x6b, 0x20]);
+	// Wake device up
+	bus.write(&[0x6b, 0x20])
+}
 
-	bus
+#[derive(Debug)]
+struct MPUSample {
+	accel: [f32; 3],
+	temp: f32,
+	gyro: [f32; 3],
+}
+
+fn parse_sample(buf: [u8; (3 + 1 + 3) * 2]) -> Result<MPUSample, io::Error> {
+	let mut rdr = io::Cursor::new(buf);
+	Ok(MPUSample {
+		accel: [
+			(try!(rdr.read_i16::<BigEndian>()) as f32) / 16384.0,
+			(try!(rdr.read_i16::<BigEndian>()) as f32) / 16384.0,
+			(try!(rdr.read_i16::<BigEndian>()) as f32) / 16384.0,
+		],
+		temp: (try!(rdr.read_i16::<BigEndian>()) as f32) / 340.0 + 35.0,
+		gyro: [
+			(try!(rdr.read_i16::<BigEndian>()) as f32) / 131.0,
+			(try!(rdr.read_i16::<BigEndian>()) as f32) / 131.0,
+			(try!(rdr.read_i16::<BigEndian>()) as f32) / 131.0,
+		],
+	})
+}
+
+fn read_sample<E: Error + From<io::Error>>(bus: &mut I2CDevice<Error=E>) -> Result<MPUSample, E> {
+	let mut buf = [0u8; (3 + 1 + 3) * 2];
+	try!(read_reg(bus, 0x3b, &mut buf));
+	parse_sample(buf).map_err(Into::into)
 }
 
 fn main() {
-	let mut bus = setup();
+	let dev = env::args().nth(1).unwrap();
+	let mut bus = LinuxI2CDevice::new(dev, 0x68).unwrap();
 
-	bus.write(&[0x3b]);
-	let mut buf = [0u8; 6];
-	bus.read(&mut buf);
+	setup(&mut bus).unwrap();
 
-	let mut rdr = Cursor::new(buf);
-	for _ in 0..3 {
-		let v = rdr.read_i16::<BigEndian>().unwrap();
-		println!("{}", (v as f32) / 16384.0);
+	if let Ok(sample) = read_sample(&mut bus) {
+		println!("{:?}", sample);
 	}
 }
